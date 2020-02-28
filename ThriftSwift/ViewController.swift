2be55a9e8f9dbcfc7715399f3082d15d86cb6a0d
FreakyCoder <kurayogun@gmail.com>
2020-02-28 23:12:50 +0300

    Initial Commit

---
+++ ThriftSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ThriftSwift
+//
+//  Created by Kuray on 28.02.2020.
+//  Copyright © 2020 Coursion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
